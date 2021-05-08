2dadb51d2094996d79422a6eaab521a8814f5d83
daniil-svetashov <79439962+daniil-svetashov@users.noreply.github.com>
2021-05-08 22:06:25 +0300

    Update praktfiles_result_txt.c

--- praktfiles_result_txt.c
+++ praktfiles_result_txt.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
+#include <windows.h>
 #include <string.h>
 
 int main()
 {
-    setlocale(LC_ALL,"rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     FILE *f1,*f2;
 
-    char text[100];
-    char ch;
+    char text[150],ch;
     int kod;
     puts("Пожалуйста сделайте выбор:");
     puts("0 - Воспользоваться шаблоном текста, удовлетворяющим условию задания");
     puts("1 - Ввести свой текст по желанию");
+
     do
     {
         scanf("%d",&kod);
@@ -27,26 +28,26 @@ int main()
     }
     while(kod!=0&&kod!=1);
 
+
       if((f1=fopen("1.txt","w"))==NULL)
       {
           printf("Открыть файл 1.txt для записи не получилось, ошибка!");
           return 1;
       }
 
-
-
       fputs(text,f1);
       fclose(f1);
 
-      if((f2=fopen("2.txt","w"))==NULL)
-         {
-          printf("Открыть файл 2.txt для записи не получилось, ошибка!");
-          return 2;
-         }
+
      if((f1=fopen("1.txt","r"))==NULL)
          {
           printf("Открыть файл 1.txt для чтения не получилось, ошибка!");
-          return 3;
+          return 1;
+         }
+     if((f2=fopen("2.txt","w"))==NULL)
+         {
+          printf("Открыть файл 2.txt для записи не получилось, ошибка!");
+          return 2;
          }
 
       while((ch=fgetc(f1))!=EOF)
@@ -59,16 +60,17 @@ int main()
          else
          putc(ch,f2);
       }
-      fclose(f1);
-      fclose(f2);
+      fclose(f2);//закрываем, чтобы открыть для чтения,f1 оставляем открытым.
       rewind(f1);
 
       if((f2=fopen("2.txt","r"))==NULL)
       {
-          printf("Открыть файл 1.txt для чтения не получилось, ошибка!");
-          return 4;
+          printf("Открыть файл 2.txt для чтения не получилось, ошибка!");
+          return 2;
       }
 
+      //Вывод второго файла на экран.
+      puts("Текст во втором файле с запятыми вместо пробелов");
       while((ch=fgetc(f2))!=EOF)
       {
           putchar(ch);
@@ -76,38 +78,44 @@ int main()
       fclose(f2);
       rewind(f2);
 
-     //Дописываем  символы отлоичыне от цифр
 
 
+     /////////////////////////////////////////////////////
+     //Дописываем  символы во второй файл,отличные от цифр
 
-     if((f2=fopen("2.txt","a"))==NULL)//открываем для записи символов отличных от цифр
+     if((f2=fopen("2.txt","a"))==NULL)//открываем для записи символов отличных от цифр в конец файла
+         {
+          printf("Открыть файл 2.txt для добавления текста в конец не получилось, ошибка!");
+          return 2;
+         }
+
+
+      while((ch=fgetc(f1))!=EOF)
+      {
+        if(ch<=47 || ch>=58)
+        {
+         putc(ch,f2);
+        }
+      }
+      fclose(f1);
+      fclose(f2);
+      rewind(f1);
+
+
+       printf("\n\n\n");
+       if((f2=fopen("2.txt","r"))==NULL)//открываем для записи символов отличных от цифр
          {
           printf("Открыть файл 2.txt для записи не получилось, ошибка!");
           return 2;
          }
 
-     if((f1=fopen("1.txt","r"))==NULL)
-         {
-          printf("Открыть файл 1.txt для чтения не получилось, ошибка!");
-          return 3;
-         }
-
-      while((ch=fgetc(f1))!=EOF)
-      {
-        if(ch>47&&ch<58)
-        {
-
-        }
-         else
-         putc(ch,f2);
-      }
-
+      puts("Текст во втором файле с добавленым текстом без цифр первого файла:");
       while((ch=fgetc(f2))!=EOF)
       {
           putchar(ch);
       }
-      fclose(f1);
       fclose(f2);
+      rewind(f2);
 
 
     return 0;
